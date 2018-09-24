2e5a9d17d4d99604557275f356436c8697583459
imranshad <imran.shad@ymail.com>
2018-09-24 15:04:13 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<head>
+    <title>Contact Management System</title>
+</head>
+<body>
+<div>
+
+</div>
+</body>
+
+</html>
+
+
+
+<?php
+require_once './content/head.html';
+require_once './content/left_side_bar.html';
+require_once './content/container.html'
+?>
